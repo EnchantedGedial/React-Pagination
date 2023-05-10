05335e61bdde71aedfedccfd04bfa94d5a2ed7bb
Jayanth <jay.codmik.18@gmail.com>
2023-05-10 14:49:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [products,setProducts]=  useState([])
+  const [page,setPage]= useState(2)
+
+  const change= (t)=>{
+    setPage(t)
+
+
+  }
+
+  const fetchproducts= async () => {
+
+    let res = await fetch(`https://dummyjson.com/products?limit=100`)
+    let data =  await res.json()
+
+
+    if(data && data.products){
+
+      setProducts(data.products);
+    }
+    
+  }
+  console.log(products);
+useEffect(() => {
+  fetchproducts()
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+    {products.slice(page*10-10,page*10).map((items)=>{
+     return <div>
+        <img src={ items.thumbnail}  alt="img-product" />
+        <div> Product :{ items.title} </div>
+        <div>Price : { items.price}  </div>
+        <div> Rating : { items.rating}  </div>
+
+      </div>
+
+    })}
+
+
+    <div className="paginationBtns"  style={{display:'flex' , margin:'200px'}}>
+      <div className="left">⬅️</div  >
+      {[...Array(products.length/10)].map((_,indx)=>{
+        return <div onClick={()=>change(indx+1)}  style={{margin:'10px'}}>
+          {indx+1}
+
+          </div>
+      })}
+      <div className="right">▶️</div>
     </div>
+
+
+    </>
   );
 }
 
 export default App;
+
+
